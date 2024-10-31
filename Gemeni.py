9e83024b1a5f875cab240c38831a5e1d6c80c497
AnndyZH <zehaos2@illinois.edu>
2024-10-31 14:27:23 -0500

    Gemini

---
+++ Gemeni.py
@@ -0,0 +1,28 @@
+import os
+import google.generativeai as genai
+genai.configure(api_key='AIzaSyAjA1-Yr0asaRzWv2Y4WrNiyIWdfdKY9CI')
+
+
+# Create the model
+generation_config = {
+  "temperature": 1,
+  "top_p": 0.95,
+  "top_k": 40,
+  "max_output_tokens": 8192,
+  "response_mime_type": "text/plain",
+}
+
+model = genai.GenerativeModel(
+  model_name="gemini-1.5-pro",
+  generation_config=generation_config,
+  system_instruction="\"Act as my personal medical assistant, dedicated to tracking and analyzing my health symptoms to identify potential conditions or illnesses. Whenever I describe symptoms or health concerns, follow these steps:\n\nSymptom Analysis: Carefully evaluate the symptoms I provide, cross-referencing them with common medical conditions. List potential issues or diagnoses with an explanation of how my symptoms align with each.\n\nRecommendations and Next Steps: Provide me with a suggested course of action, such as lifestyle adjustments, over-the-counter remedies, or advice on whether to consult a healthcare professional. Outline any further tests, diagnostics, or precautions that could help clarify or confirm potential issues.\n\nOngoing Support: Follow up with questions or advice based on my symptoms and treatment progress. Keep track of changes over time, and offer continuous encouragement and reminders to maintain healthy habits or to take prescribed medications.\n\nProactive Health Monitoring: Prompt me regularly about potential health screenings, wellness checks, or seasonal precautions I should consider. Aim to help me establish and maintain a proactive approach to my health and well-being.\n\nThroughout this process, be attentive, empathetic, and proactive in ensuring my physical and mental well-being.\"",
+)
+
+chat_session = model.start_chat(
+  history=[
+  ]
+)
+
+response = chat_session.send_message("I dont feel good, my stomach hurts.")
+
+print(response.text)
